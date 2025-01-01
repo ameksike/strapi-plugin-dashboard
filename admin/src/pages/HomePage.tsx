0cbfe2ea23d37fc5bf4b19eea0b025b201011a0b
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-01-01 15:46:46 +0100

    update the home page

--- admin/src/pages/HomePage.tsx
+++ admin/src/pages/HomePage.tsx
@@ -1,14 +1,59 @@
-import { Main } from '@strapi/design-system';
+import { Main, Tooltip, Grid, EmptyStateLayout, Box, Loader, Typography } from '@strapi/design-system';
+import styled from 'styled-components';
 import { useIntl } from 'react-intl';
 
+import { PLUGIN_ID } from '../pluginId';
 import { getTranslation } from '../utils/getTranslation';
+import { useCharts } from '../service/useChart';
+import { ChartView } from '../components/ChartView';
+import Illo from '../components/Lilo';
+import ChartModal from '../components/ChartModal';
 
-const HomePage = () => {
+const StyledBox = styled(Box)`
+  border-radius: ${({ theme }) => theme.borderRadius};
+  background-color: ${({ theme }) => theme.colors.neutral100};
+  height: 80px
+`;
+
+const HomePage: React.FC = () => {
   const { formatMessage } = useIntl();
+  const { isLoading, error, charts, create, update, remove } = useCharts();
+
+  if (isLoading) {
+    return <Loader>Loading content...</Loader>
+  }
 
   return (
     <Main>
-      <h1>Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</h1>
+      <Box>
+        <StyledBox padding={3} >
+          <Typography variant="beta">Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</Typography>
+        </StyledBox >
+        <Box>
+          <Typography textColor="neutral600">This is a custom dashboard with a static value</Typography>
+        </Box>
+      </Box>
+
+      <Box>
+        <Grid.Root padding={8} >
+          {charts.map((chart) => (
+            <Grid.Item key={chart.id} col={6} s={12} xs={12} margin={2}>
+              <ChartView data={chart} />
+            </Grid.Item>
+          ))}
+
+          <Grid.Item col={4} md={6} s={12} margin={2}>
+            <Box width="100%" background="neutral100">
+              <EmptyStateLayout
+                icon={<Illo />}
+                content="You don't have any chart yet..."
+                action={
+                  <ChartModal />
+                } />
+            </Box>
+          </Grid.Item>
+        </Grid.Root>
+      </Box>
     </Main>
   );
 };
